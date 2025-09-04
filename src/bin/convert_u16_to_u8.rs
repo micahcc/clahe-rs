ed26cddf70ba75838b85a040a7a64a1a86776819
Micah Chambers (eos) <mchambers@anduril.com>
2025-09-04 13:34:06 -0700

    add example

--- src/bin/convert_u16_to_u8.rs
+++ src/bin/convert_u16_to_u8.rs
@@ -1,17 +1,21 @@
-use std::path::Path;
-
 fn main() {
-    let file = if std::env::args().count() == 2 {
-        std::env::args().nth(1).unwrap()
+    let (in_file, out_file) = if std::env::args().count() == 3 {
+        (
+            std::env::args().nth(1).unwrap(),
+            std::env::args().nth(2).unwrap(),
+        )
     } else {
-        panic!("Please enter an image filename")
+        panic!(
+            "Usage: {} <input> <output>",
+            std::env::args().nth(0).unwrap()
+        );
     };
 
-    let im = image::open(&Path::new(&file)).unwrap();
+    let im = image::open(&in_file).unwrap();
     let im = im.into_luma16();
     let output = clahe_rs::clahe_u16_to_u8(8, 8, 0.0, &im).unwrap();
 
     output
-        .save_with_format(&Path::new("output.png"), image::ImageFormat::Png)
+        .save_with_format(&out_file, image::ImageFormat::Png)
         .unwrap();
 }
